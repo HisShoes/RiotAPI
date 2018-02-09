1d061fe78e6014a1e2b4b3cd4a4ce02ee7a43db9
Ryan Simpson <ryan999000999@hotmail.co.uk>
2018-02-09 14:08:31 +0000

    committing in working unfactored state before making changes

--- DataServer/app.js
+++ DataServer/app.js
@@ -142,7 +142,20 @@ function updateRankedCollection(league) {
     })
 }
 
-
+//gets summoner information from Riots API then stores it in MongoDB on mlab
+app.post('/pollRiotAPI', function(req, res) {
+    getSummonerByName(req.body.region, req.body.apiKey, req.body.summonerName)
+    .then(function(summoner) {
+        return getLeagueDetailsById(req.body.region, req.body.apiKey, summoner.id);
+    })
+    .then(updateRankedCollection)
+    .then(function() {
+        res.send({status: 200});
+    })
+    .catch(function(err) {
+        res.send({message: "there's a problem: " + err});
+    });
+});
 
 
 
